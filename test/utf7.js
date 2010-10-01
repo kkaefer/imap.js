35a0f9b4a0ff037744110ab76ff460a0393788ef
Konstantin Käfer <github@kkaefer.com>
2010-10-01 16:19:49 -0400

    added test for & to &- conversion

--- test/utf7.js
+++ test/utf7.js
@@ -6,10 +6,13 @@ exports['test conversion from utf8 to utf7'] = function(assert) {
   assert.equal('&ZeVnLIqe-', utf7.utf8to7('\u65E5\u672C\u8A9E'));
   assert.equal('Hi Mom -&Jjo--!', utf7.utf8to7('Hi Mom -\u263A-!'));
   assert.equal('Item 3 is &AKM-1.', utf7.utf8to7('Item 3 is \u00A31.'));
-  
+
   // Custom examples that contain more than one mode shift.
   assert.equal('Jyv&AOQ-skyl&AOQ-', utf7.utf8to7('Jyv\u00E4skyl\u00E4'));
   assert.equal('\'&T2BZfQ-\' hei&AN8-t "Hallo"', utf7.utf8to7('\'\u4F60\u597D\' heißt "Hallo"'));
+
+  // The ampersand sign is represented by &-.
+  assert.equal('Hot &- Spicy', utf7.utf8to7('Hot & Spicy'));
 };
 
 exports['test conversion from utf7 to utf8'] = function(assert) {
@@ -18,10 +21,13 @@ exports['test conversion from utf7 to utf8'] = function(assert) {
   assert.equal('\u65E5\u672C\u8A9E', utf7.utf7to8('&ZeVnLIqe-'));
   assert.equal('Hi Mom -\u263A-!', utf7.utf7to8('Hi Mom -&Jjo--!'));
   assert.equal('Item 3 is \u00A31.', utf7.utf7to8('Item 3 is &AKM-1.'));
-  
+
   // Custom examples that contain more than one mode shift.
   assert.equal('Jyv\u00E4skyl\u00E4', utf7.utf7to8('Jyv&AOQ-skyl&AOQ-'));
   assert.equal('\'\u4F60\u597D\' heißt "Hallo"', utf7.utf7to8('\'&T2BZfQ-\' hei&AN8-t "Hallo"'));
+
+  // The ampersand sign is represented by &-.
+  assert.equal('Hot & Spicy', utf7.utf7to8('Hot &- Spicy'));
 };
 
 
